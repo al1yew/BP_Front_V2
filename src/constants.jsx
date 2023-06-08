d940cb9409b2f67b8cccd54d921ed0457ab76282
al1yew <vasifja@code.edu.az>
2023-06-08 19:46:33 +0400

    commit apilink

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,5 +1,5 @@
-// export const apilink = "https://elgiz93-001-site3.htempurl.com/api";
-export const apilink = "http://localhost:37234/api";
+export const apilink = "https://elgiz93-001-site3.htempurl.com/api";
+// export const apilink = "http://localhost:37234/api";
 
 export const SET_DATA = "SET_DATA";
 export const SET_ASSESSMENTS_AFTER_DELETE = "SET_ASSESSMENTS_AFTER_DELETE";
